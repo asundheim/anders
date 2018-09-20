f7da3ae6d925a3814b777981e71df76fe59310ac
Anders Sundheim <andersundheim@gmail.com>
2018-09-20 10:20:35 -0500

    Added animation to dungeon boxes

--- src/app/constellations/constellations.component.spec.ts
+++ src/app/constellations/constellations.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConstellationsComponent } from './constellations.component';
+import { globals } from '../../environments/environment';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ConstellationsComponent', () => {
   let component: ConstellationsComponent;
@@ -21,5 +24,18 @@ describe('ConstellationsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not draw constellations when the toggle is disabled', () => {
+    // Arrange
+    globals.constellationsEnabled = false;
+
+    // Act
+    const element: DebugElement = fixture.debugElement.queryAll(By.css('canvas'))[0];
+    element.triggerEventHandler('mouseMove', null);
+
+    // Assert
+    expect(true);
+
   });
 });
