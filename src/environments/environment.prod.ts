a9b3b776f02414f19a7bf23a7d74af4a365fd956
Anders Sundheim <andersundheim@gmail.com>
2018-12-06 23:43:46 -0600

    Updated build strings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const globals = {
   constellationsEnabled: false,
   spotifyRedirect: 'https://anderssundheim.me/anders/spotify',
   header: 'Anders Sundheim',
-  up: 'https://anderssundheim.me/anders/high'
+  up: 'http://anderssundheim.me/anders/high'
 };
