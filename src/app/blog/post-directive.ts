b2c5dac94f2639ceac514d1989eed987c158cbb8
asundheim <asundheim@optimalblue.com>
2019-07-29 14:22:13 -0500

    removed old files

--- src/app/blog/post-directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-  selector: '[blog-post]',
-})
-export class PostDirective {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
