d4360929131379da3c12fef87f7777824e24bc2d
Anders Sundheim <andersundheim@gmail.com>
2021-06-11 13:36:06 -0700

    lazy

--- src/app/chess/chess.service.ts
+++ src/app/chess/chess.service.ts
@@ -20,5 +20,58 @@ export class ChessService {
   getPuzzleHistory(user: string): Observable<IChessHistory[]> {
     return this.http.get<IChessHistory[]>(`${this.baseURL}/api/user/${user}/rating-history`);
   }
+const fs = require('fs');
+const readline = require('readline');
+let dryStreak = 0;
+let cdf = 0;
+let dryPoints = [];
+let dryStreaks = [];
+
+function factorial(num)
+{
+    var rval=1;
+    for (var i = 2; i <= num; i++)
+        rval = rval * i;
+    return rval;
+}
+
+function chance(num) {
+    return (num/570000)*.8
+}
+
+function totalPoints() {
+    return dryPoints.reduce((a,b) => a+b, 0);
+}
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('raid_tracker_data.log'),
+    crlfDelay: Infinity
+});
+console.log('Chambers of Xeric dry streaks for thecrinkler')
+
+rl.on('line', (line) => {
+    const data = JSON.parse(line);
+    dryStreak++;
+    dryPoints.push(data.totalPoints);
+    if (data.specialLoot) {
+        total = 0;
+        for (let i = 0; i < 1; i++) {
+            total += 1 - (factorial(dryPoints.length)/(factorial(i)*(factorial(dryPoints.length - i))))*Math.pow((chance(dryPoints[i])), i)*Math.pow(1-(chance(dryPoints[i])),dryPoints.length - i);
+        }
+
+        dryStreaks.push({streak: dryStreak, loot: data.specialLoot, receiver: data.specialLootReceiver});
+        console.log(`Dry Streak: ${dryStreak} - broken by ${data.specialLoot} from ${data.specialLootReceiver} - probability of purple ${total * 100}% (${totalPoints()} points)`);
+        dryStreak = 0;
+        dryPoints = [];
+    }
+}).on('close', () => {
+    total = 0;
+    for (let i = 0; i < 1; i++) {
+        total += 1 - (factorial(dryPoints.length)/(factorial(i)*(factorial(dryPoints.length - i))))*Math.pow((chance(dryPoints[i])), i)*Math.pow(1-(chance(dryPoints[i])),dryPoints.length - i);
+    }
+    console.log(`Current Dry Streak: ${dryStreak} - probability of purple ${total * 100}% (${totalPoints()} points)`);
+});
+
+
 
 }
